0253f71c537a6fe4759fb0d55778f540041c3715
Nikita <nikitapohod@icloud.com>
2024-10-07 14:58:20 +0300

    try to add webhook

--- run.py
+++ run.py
@@ -1,22 +1,62 @@
-import asyncio
 import logging
 import os
+import sys
+
 from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
+from aiohttp import web
+
 from app.handlers import router
 
 TOKEN = os.getenv('TOKEN')
 
 bot = Bot(token=TOKEN)
-dp = Dispatcher()
+
+WEBHOOK_PATH = f"/bot/{TOKEN}"
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")
 
 
-async def main():
+async def on_startup(bot: Bot) -> None:
+    # If you have a self-signed SSL certificate, then you will need to send a public
+    # certificate to Telegram
+    await bot.set_webhook(WEBHOOK_URL)
+
+
+def main() -> None:
+    # Dispatcher is a root router
+    dp = Dispatcher()
+    # ... and all other routers should be attached to Dispatcher
     dp.include_router(router)
-    await dp.start_polling(bot)
 
-if __name__ == '__main__':
+    # Register startup hook to initialize webhook
+    dp.startup.register(on_startup)
+
+    # Initialize Bot instance with default bot properties which will be passed to all API calls
+    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+
+    # Create aiohttp.web.Application instance
+    app = web.Application()
+
+    # Create an instance of request handler,
+    # aiogram has few implementations for different cases of usage
+    # In this example we use SimpleRequestHandler which is designed to handle simple cases
+    webhook_requests_handler = SimpleRequestHandler(
+        dispatcher=dp,
+        bot=bot,
+        secret_token=TOKEN,
+    )
+    # Register webhook handler on application
+    webhook_requests_handler.register(app, path=WEBHOOK_PATH)
+
+    # Mount dispatcher startup and shutdown hooks to aiohttp application
+    setup_application(app, dp, bot=bot)
+
+    # And finally start webserver
+    web.run_app(app, host='127.0.0.1', port=8080)
+
+
+if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
-    try:
-        asyncio.run(main())
-    except KeyboardInterrupt:
-        print('exit')
+    main()
